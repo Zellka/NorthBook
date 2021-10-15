73451cd2f58647340e2d7e78cf07d1f2d0737cab
sonya <sonp2401@gmail.com>
2021-10-15 21:11:14 +0300

    add registration scrin

--- app/src/main/java/com/example/northbook/InputFragment.kt
+++ app/src/main/java/com/example/northbook/InputFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 
 class InputFragment : Fragment() {
 
@@ -16,6 +19,12 @@ class InputFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.input_text).setOnClickListener {
+            findNavController().navigate(R.id.action_inputFragment_to_registrationFragment)
+        }
 
+        view.findViewById<Button>(R.id.fragment_input_button).setOnClickListener {
+            findNavController().navigate(R.id.action_inputFragment_to_registrationFragment)
+        }
     }
 }
