49f0de3a2df75d6f4fce0e903299723827c33447
sonya <sonp2401@gmail.com>
2021-10-16 21:09:12 +0300

    add book avas

--- app/src/main/java/com/example/northbook/ui/category/TrendsFragment.kt
+++ app/src/main/java/com/example/northbook/ui/category/TrendsFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.northbook.R
 import com.example.northbook.adapter.BookAdapter
 import com.example.northbook.adapter.ClickListener
 import com.example.northbook.databinding.FragmentTrendsBinding
@@ -28,25 +29,31 @@ class TrendsFragment : Fragment(), ClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val books = listOf<Book>(
-            Book("Название1", "Автор1", 1),
-            Book("Название2", "Автор2", 2),
-            Book("Название3", "Автор3", 3),
-            Book("Название4", "Автор4", 4),
-            Book("Название1", "Автор1", 1),
-            Book("Название2", "Автор2", 2),
-            Book("Название3", "Автор3", 3),
-            Book("Название4", "Автор4", 4),
-            Book("Название1", "Автор1", 1),
-            Book("Название2", "Автор2", 2),
-            Book("Название3", "Автор3", 3),
-            Book("Название4", "Автор4", 4),
-            Book("Название1", "Автор1", 1),
-            Book("Название2", "Автор2", 2),
-            Book("Название3", "Автор3", 3),
-            Book("Название4", "Автор4", 4)
+            Book("Руководство к действию на ближайшие дни", "Й. Блум", 4),
+            Book("Ход королевы", "У. Тевис", 4),
+            Book("Дюна", "Ф. Герберт", 5),
+            Book("Снежная сказка", "А. Полярный", 2),
+            Book("Гарри Поттер", "Д. Роулинг", 5),
+            Book("Завтрак у Тиффани", "Т. Капоте", 5),
+            Book("Ночной дозор", "С. Лукьяненко", 5),
+            Book("Портрет Дориана Грея", "О. Уайлд", 5),
+            Book("Идиот", "Ф. Достоевский", 1)
         )
+
+        val list = listOf<Int> (
+            R.drawable.bb1,
+            R.drawable.bb2,
+            R.drawable.bb3,
+            R.drawable.bb4,
+            R.drawable.bb5,
+            R.drawable.bb6,
+            R.drawable.bb7,
+            R.drawable.bb8,
+            R.drawable.bb9
+        )
+
         binding.recyclerTrends.layoutManager = GridLayoutManager(this.requireContext(), 3)
-        binding.recyclerTrends.adapter = BookAdapter(books, this)
+        binding.recyclerTrends.adapter = BookAdapter(books, this,list)
     }
 
     override fun showBook(book: Book) {
